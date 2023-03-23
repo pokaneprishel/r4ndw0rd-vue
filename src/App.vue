e392499450e24e6a540863eb8274cf9e39086814
Danil Mazitov <krimskyxd@gmail.com>
2023-03-23 21:29:15 +0300

    fix - debugging

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
     <!-- Сетка приложения -->
     <div class="wrapper">
       <!-- Заголовок -->
-      <h1 ref="title text-5xl">Create strong passwords quickly and easily</h1>
+      <h1 ref="title">Create strong passwords quickly and easily</h1>
       <!-- Инпут для генерации пароля -->
       <section class="password-box">
         <input type="text" class="password-input" v-model="password" readonly />
